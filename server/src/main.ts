bfe6ebe35938aa5294bb91a38504050a0083c143
Henri Kim Pinheiro Rodrigues <hkimpr40@gmail.com>
2025-07-29 17:14:37 -0300

    create server test

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,1 +1,15 @@
-console.log("test");
+import Express from 'express';
+
+async function main() {
+    const App = Express();
+    
+    App.get("/", async (req, res)=>{
+        res.status(200).send("test");
+    });
+
+    App.listen(8080, async ()=>{
+        console.log("Server listening on port 8080 [alt HTTP]");
+    });
+}
+
+main();
